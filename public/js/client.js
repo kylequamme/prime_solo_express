79da1a8a1a3e933f65334f3e1d26ddc03f7f9fa5
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-07-07 14:31:34 -0500

    removed console log

--- public/js/client.js
+++ public/js/client.js
@@ -2,7 +2,7 @@ $(function(){
   $('.btn').on('click', function(){
     $('.balance').remove();
     $.get('/balance').then(function(response){
-      console.log(response);
+      // console.log(response);
       $('main').append('<div class="balance"><h1>' +
       response.text + '</h1>' + '<h2>' + response.money
       + '</h2></div>');
